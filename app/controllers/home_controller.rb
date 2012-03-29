9c476718524218ce3b4c7391a789ee881af02c4b
Ross Kaffenberger <rosskaff@gmail.com>
2012-03-29 14:29:38 -0400

    Only display enabled jobs via home#show

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def show
     flash[:notice] = warden.message if warden.message
-    @jobs = JobDecorator.decorate(Job.order(:name))
+    @jobs = JobDecorator.decorate(Job.enabled.order(:name))
   end
 
 end
